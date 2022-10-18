3b8d9d6e13fcc2d7e09b2096048da46ca40b6960
Hojjat Jokar <paintthedream@gmail.com>
2022-10-18 18:00:00 +0200

    feat(button): add basic button

--- libs/components/src/lib/button/button.stories.tsx
+++ libs/components/src/lib/button/button.stories.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { Button }  from './button';
+
 
 const Story: ComponentMeta<typeof Button> = {
   component: Button,
@@ -8,8 +10,26 @@ const Story: ComponentMeta<typeof Button> = {
 };
 export default Story;
 
-
-const Template: ComponentStory< typeof Button > = (args) => <Button {...args} />;
+const theme = {
+  primaryColor: '#003060',
+  secondaryColor: '#fff',
+  'primary-600': '#002A54',
+  'primary-700': '#002448',
+  fontFamily: 'Open Sans',
+  typeSizeBase: '16px',
+  'size-1': '8px',
+  'size-2': '10px',
+  'size-3': '14px',
+  'size-4': '24px',
+  'size-5': '42px',
+  'size-6': '68px',
+  dropShadow: '0 3px 10px rgb(0 0 0 / 0.2)',
+}
+const Template: ComponentStory< typeof Button > = (args) => (
+  <ThemeProvider theme={theme}>
+    <Button {...args} />
+  </ThemeProvider>
+);
 
 export const Primary = Template.bind({})
 
--- libs/components/src/lib/button/button.tsx
+++ libs/components/src/lib/button/button.tsx
@@ -1,16 +1,52 @@
 import React from 'react';
-// import styled from 'styled-components';
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  border-radius: 8px;
+  color: ${(props) => props.theme.secondaryColor};
+  background-color: ${(props) => props.theme.primaryColor};
+  padding: ${(props) => props.theme['size-4']};
+  font-size: ${(props) => props.theme.typeSizeBase};
+  font-weight: bold;
+  font-family: ${(props) => props.theme.fontFamily};
+  border: none;
+  box-sizing: border-box;
+
+  &:hover {
+    background-color: ${(props) => props.theme['primary-600']};
+  }
+
+  &:focus {
+    background-color: ${(props) => props.theme['primary-600']};
+    box-shadow: ${(props) => props.theme.dropShadow};
+    outline: none;
+  }
+
+  &:active{
+    background-color: ${(props) => props.theme['primary-700']};
+  }
+
+  color: ${props => {
+    console.log('them', props.theme)
+    return '#fff'
+  }};
+
+`;
 
 type ButtonProps = {
   label: string;
+  disabled?: boolean;
+  size?: 'large' | 'small';
+  type?: 'primary' | 'secondary';
+  iconName?: string;
 }
 
-export function Button({ label }: ButtonProps) {
-  // return null
+export function Button({ label, disabled, size = 'large', type = 'primary', iconName, ...rest  }: ButtonProps) {
+  console.log('rest', rest)
   return (
-    <button>
+    <StyledButton disabled={disabled}>
       {label}
-    </button>
+    </StyledButton>
   );
 };
 
